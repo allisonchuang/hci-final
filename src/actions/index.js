598d7ccb1b37c3189b277573967cb42bf82c3f2e
annie ke <annieke8@gmail.com>
2017-05-19 00:09:06 -0400

    reorganized files, added all rooms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,6 @@
-
-// keys for actiontypes
 export const ActionTypes = {
-  INCREMENT: 'INCREMENT',
-  DECREMENT: 'DECREMENT',
   ADD_BOOKING: 'ADD_BOOKING',
 };
-
-
-export function increment() {
-  return {
-    type: ActionTypes.INCREMENT,
-    payload: null,
-  };
-}
-
-export function decrement() {
-  return {
-    type: ActionTypes.DECREMENT,
-    payload: null,
-  };
-}
 
 export function addBooking(booking) {
   return {
